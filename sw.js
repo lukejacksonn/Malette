8dcac79b988d5d17540ce650be423f7aff0cc4f3
Travis CI <lukejacksonn@gmail.com>
2016-11-22 13:30:55 +0000

    Deploy to GitHub Pages: bf4d31bf7125cdc01902c9db149120576a0ae4e5

---
+++ sw.js
@@ -0,0 +1,30 @@
+var CACHE_NAME = 'malette-cache';
+var urlsToCache = [
+  '/',
+  '/colors.json',
+  '/main.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
