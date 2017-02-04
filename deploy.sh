17c843d3bfed813af93b058673611898aac363fc
Luke Jackson <lukejacksonn@gmail.com>
2017-02-04 17:14:19 -0500

    Renames out to public

--- deploy.sh
+++ deploy.sh
@@ -16,21 +16,21 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-# Clone the existing gh-pages for this repo into out/
+# Clone the existing gh-pages for this repo into public/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO out
-cd out
+git clone $REPO public
+cd public
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
 # Clean out existing contents
-rm -rf out/**/* || exit 0
+rm -rf public/**/* || exit 0
 
 # Run our compile script
 npm run deploy
 
 # Now let's go have some fun with the cloned repo
-cd out
+cd public
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
